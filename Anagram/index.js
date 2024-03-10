834f1121ccfc5d6b640643c9cc7edb7089d5ca79
Bhakta <dashlucky248@gmail.com>
2024-03-10 14:57:45 +0530

    updated code without using any predefined methods

--- Anagram/index.js
+++ Anagram/index.js
@@ -1,18 +1,38 @@
 // write a anagram programme
 // anagram means both the strings having the same workds with euqal length
 
-function anagramFucn(args1, args2) {
-    if (args1.length !== args2.length) return false;
+// with using predefined methods
 
-    let result1 = args1.split("").sort().join("")
-    let result2 = args2.split("").sort().join("")
+// function anagramFucn(args1, args2) {
+//     if (args1.length !== args2.length) return false;
 
-    if (result1 == result2) return true
-    return false
+//     let result1 = args1.split("").sort().join("")
+//     let result2 = args2.split("").sort().join("")
+
+//     if (result1 == result2) return true
+//     return false
+
+// }
+
+// without usinf any predefined methods
+
+function anagramFucn(args1,args2){
+    if(args1.length !== args2.length) return false;
+    let result = {}
+    for( let res of args1){
+        result[res] ? result[res] += 1 : result[res] = 1
+    }
+
+    for (let res of args2){
+        if(!result[res]) return false
+        result[res] -=1
+    }
+    return true
 
 }
 
-let str1 = 'alaleded';
-let str2 = 'laladede';
+
+let str1 = 'anagram';
+let str2 = 'gramana';
 
 console.log(anagramFucn(str1, str2))
