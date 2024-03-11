3a8a6dbf71ddbb1dcd9f4c4b24dbe7c5a59951de
Bhakta <dashlucky248@gmail.com>
2024-03-11 16:35:14 +0530

    updated the code using pure recursion

--- collect odd numbers using helper methods/script.js
+++ collect odd numbers using helper methods/script.js
@@ -14,17 +14,29 @@
 
 // using recursion
 
+// function collectOddNum(arg) {
+//     let result = [];
+//     function helperFucn(helperArg) {
+//         if (helperArg.length === 0) return
+//         if(helperArg[0] % 2 !==0){
+//             result.push(helperArg[0])
+//         }
+//         helperFucn(helperArg.slice(1))
+//     }
+//     helperFucn(arg)
+//     return result;
+// }
+
+// using pure recursion
+
 function collectOddNum(arg) {
-    let result = [];
-    function helperFucn(helperArg) {
-        if (helperArg.length === 0) return
-        if(helperArg[0] % 2 !==0){
-            result.push(helperArg[0])
-        }
-        helperFucn(helperArg.slice(1))
+    let result = []
+    if (arg.length === 0) return result;
+
+    if (arg[0] % 2 !== 0) {
+        result.push(arg[0])
     }
-    helperFucn(arg)
-    return result;
+    return result = result.concat(collectOddNum(arg.slice(1)))
 }
 
 let arr = [1, 23, 4, 45]
