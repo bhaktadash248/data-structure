53d311dfcfa5ca6661df4077d047a33bbd228fc4
Bhakta <dashlucky248@gmail.com>
2024-03-14 22:15:13 +0530

    bubble sort

---
+++ bubble sort/script.js
@@ -0,0 +1,17 @@
+// bubble sort 
+
+function bubbleSort(arg) {
+    for (let i = arg.length; i > 0; i--) {
+        for (let j = 0; j < i-1; j++) {
+            if (arg[j] > arg[j + 1]) {
+                var tempVar = arg[j];
+                arg[j] = arg[j + 1]
+                arg[j + 1] = tempVar
+            }
+        }
+
+    }
+    return arg
+}
+const arr = [5, 7, 9, 6, 4, 8, 1, 3]
+console.log(bubbleSort(arr))
