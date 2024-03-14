fb60e287cb71f4e9260fa3f505c7eba4504fba30
Bhakta <dashlucky248@gmail.com>
2024-03-14 23:25:00 +0530

    selection search

--- bubble sort/script.js
+++ bubble sort/script.js
@@ -2,14 +2,14 @@
 
 function bubbleSort(arg) {
     for (let i = arg.length; i > 0; i--) {
-        for (let j = 0; j < i-1; j++) {
+        for (let j = 0; j < i - 1; j++) {
             if (arg[j] > arg[j + 1]) {
-                var tempVar = arg[j];
+                var tempVar = arg[j]
                 arg[j] = arg[j + 1]
                 arg[j + 1] = tempVar
+
             }
         }
-
     }
     return arg
 }
---
+++ selection sort/script.js
@@ -0,0 +1,21 @@
+// selection sort
+
+function selectioSort(arg) {
+    for (let i = 0; i < arg.length; i++) {
+        for (j = i + 1; j < arg.length; j++) {
+            var lowest = i
+            if (arg[i] > arg[j]) {
+                lowest = j
+            }
+        }
+        if (i !== j) {
+            var tempVar = arg[i]
+            arg[i] = arg[lowest]
+            arg[lowest] = tempVar
+        }
+
+    }
+    return arg
+}
+const arr = [3, 7, 2, 8, 4, 9, 6]
+console.log(selectioSort(arr))
