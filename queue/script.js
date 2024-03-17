33d893cec366df1ebb3ab5ef649434a491425792
Bhakta <dashlucky248@gmail.com>
2024-03-17 23:16:33 +0530

    initial commit

---
+++ queue/script.js
@@ -0,0 +1,7 @@
+/*
+    What is A Queue?
+
+    Queue is the first in first out i.e FIFO.
+    1. Printing in a printer
+    
+*/
---
+++ stacks/script.js
@@ -0,0 +1,6 @@
+/* 
+Stack means LIFO(Last input first output)
+We are using it for the history and we can say the array push and pop, unshift and shift 
+are the best example of stack
+in the case of pop it will remove the last element which was added at the last and same for the shift.
+*/
