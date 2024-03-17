25372a903c3383febdd6591f25f22f4aefb51286
Bhakta <dashlucky248@gmail.com>
2024-03-17 22:00:42 +0530

    shift and unshift

--- doubleLinklist/script.js
+++ doubleLinklist/script.js
@@ -41,6 +41,35 @@ class DoubleLinkList {
         this.length--
         return popTail
     }
+
+    shift(){
+        if(!this.head) return undefined;
+        var newHead = this.head;
+        if(this.length ===1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = newHead.next;
+            this.head.prev = null
+            newHead.next = null
+        }
+        this.length--;
+        return this
+    }
+
+    unshift(val){
+        var newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode
+        }else{
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode
+        }
+        this.length++;
+        return this
+    }
 }
 
 var List = new DoubleLinkList()
